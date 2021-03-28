178d71d84ddcd5813fc94fd22c775560333b7890
Bas Kaal <3askaal@gmail.com>
2021-03-28 16:41:21 +0200

    trying to achieve masonry with pure CSS for sick animations

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="gallery" v-masonry transition-duration="1s" gutter=".gallery__gutter">
-    <div class="gallery__item" v-masonry-tile v-for="(image, imageIndex) of images" :key="imageIndex">
-      <div class="gallery__gutter" />
+  <div class="gallery">
+    <div class="gallery__item" v-for="(image, imageIndex) of images" :key="imageIndex">
       <img :src="image" />
+      <div class="gallery__item__gutter" />
     </div>
   </div>
 </template>
@@ -23,25 +23,41 @@ export default {
   overflow: hidden;
   max-width: 1200px;
   margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
+  // display: flex;
+  // justify-content: space-between;
+
+  // display: grid;
+  // gap: 1rem;
+  // grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+  // grid-template-rows: masonry;
+  column-fill: initial;
+  column-gap: 1rem;
+  column-count: 4;
+  height: calc(1200px + 2rem);
 }
 
-.gallery__gutter {
-  width: 1rem;
-  display: block;
-}
+// .gallery__gutter {
+//   width: 1rem;
+//   display: block;
+// }
 
 .gallery__item {
-  max-width: calc(25% - 1rem);
-  flex: 0 1 auto;
+  // max-width: calc(25% - 1rem);
+  // flex: 0 1 auto;
   object-fit: cover;
   object-position: center center;
-  margin-bottom: 1rem;
+  // margin-bottom: 1rem;
+  overflow: hidden;
 
   img {
     display: block;
     width: 100%;
   }
+}
+
+.gallery__item__gutter {
+  display: inline-block;
+  background-color: $red;
+  width: 100%;
 }
 </style>
